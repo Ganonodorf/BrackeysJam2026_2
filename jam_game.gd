210c5b58caafbd8eb15169e688a243b7580417b5
Ganonodorf <juanjogarridobascaran@gmail.com>
2026-08-24 19:55:38 +0200

    feat: add new box

--- jam_game.gd
+++ jam_game.gd
@@ -3,8 +3,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start('test-timeline')
-	get_viewport().set_input_as_handled()
+	#Dialogic.start('test-timeline')
+	#get_viewport().set_input_as_handled()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

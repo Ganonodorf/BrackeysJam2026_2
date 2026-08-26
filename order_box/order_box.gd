7274e0d10136b4c06f802ae28643826f64ee1a9b
Ganonodorf <juanjogarridobascaran@gmail.com>
2026-08-26 12:52:48 +0200

    feat: add order boxes and check if it is complete

---
+++ order_box/order_box.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+class_name order_box
+
+@export var order: Array[Pickable]
+var inside: Array[Pickable]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(_is_order_completed()):
+		print("estoy completo")
+	else:
+		print("No estoy completo")
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if(body.is_in_group("pickable")):
+		inside.append(body)
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if(body.is_in_group("pickable")):
+		inside.erase(body)
+
+func _is_order_completed() -> bool:
+	return _inside_number_is_equal_to_order() && _inside_contains_same_elements_than_order()
+	
+func _inside_number_is_equal_to_order() -> bool:
+	return order.size() == inside.size()
+
+func _inside_contains_same_elements_than_order() -> bool:
+	for pickable in inside:
+		if(!order.has(pickable)): return false
+		if(inside.count(pickable) != order.count(pickable)): return false
+	return true
